ab682812b42b922e4b11bb1a15852122f5d0b48b
Duke Yin <yduke@hotmail.com>
2021-06-06 16:43:17 +0800

    music meta field update
    deleted the old genre and artists field.
    Updated translations and typos.

--- includes/meta-field.php
+++ includes/meta-field.php
@@ -11,26 +11,20 @@ function music_metaboxes() {
 		'priority'      => 'low',
 		'show_names'    => true, 
 	) );
-//
 
 	$cmb->add_field( array(
 		'name' => esc_html__( 'Music file', 'duke-yin-helper' ),
 		'desc' => esc_html__( 'The music file or URL for the song, if the URL is external for your website, obtaining information from file may fail due to the CORS policy.', 'duke-yin-helper' ),
 		'id'   => '_music_file',
 		'type' => 'file',
+		'text'    => array('add_upload_file_text' => esc_html__( 'Upload file', 'duke-yin-helper' )),
 		'after_field' => '<a id="getid3" class="button button-primary button-large">'.esc_html__( 'Get info from file', 'duke-yin-helper' ).'</a><span class="spinner"></span>',
+		
 		'query_args' => array(
 			'type' => array(
 				'audio',
 			),
 		),
-	) );
-
-	$cmb->add_field( array(
-		'name' => esc_html__( 'Contributing Artists', 'duke-yin-helper' ),
-		'desc' => esc_html__( 'The artist(s) who made contributes to this song.', 'duke-yin-helper' ),
-		'id'   => '_artists',
-		'type' => 'text',
 	) );
 	
 	$cmb->add_field( array(
@@ -42,9 +36,10 @@ function music_metaboxes() {
 
 	$cmb->add_field( array(
 		'name' => esc_html__( 'Album Art', 'duke-yin-helper' ),
-		'desc' => esc_html__( 'The album name that this music belong to.', 'duke-yin-helper' ),
+		'desc' => esc_html__( 'The album artwork.', 'duke-yin-helper' ),
 		'id'   => '_album_art',
 		'type' => 'file',
+		'text'    => array('add_upload_file_text' => esc_html__( 'Upload file', 'duke-yin-helper' )),
 		'query_args' => array(
 		'type' => array('image/gif','image/jpeg','image/png',),
 		'preview_size' => 'small',
@@ -52,18 +47,16 @@ function music_metaboxes() {
 	) );
 
 	$cmb->add_field( array(
-		'name' => esc_html__( 'Genre', 'duke-yin-helper' ),
-		'desc' => esc_html__( 'The genre name(s) that this music belong to.', 'duke-yin-helper' ),
-		'id'   => '_genre',
-		'type' => 'text',
-	) );
-	
-	$cmb->add_field( array(
 		'name' => esc_html__( 'Year', 'duke-yin-helper' ),
-		'desc' => esc_html__( 'The year this music was created.', 'duke-yin-helper' ),
+		'desc' => esc_html__( 'The year this music was released.', 'duke-yin-helper' ),
 		'id'   => '_year',
 		'type' => 'text_date_timestamp',
 		'date_format' => 'Y',
+		'attributes' => array(
+			'data-datepicker' => json_encode( array(
+				'yearRange' => '-100:+0',
+			) ),
+		),
 	) );
 	
 	$cmb->add_field( array(
@@ -71,6 +64,7 @@ function music_metaboxes() {
 		'desc' => esc_html__( 'The lyrics file url of this song, accept .lrc file.', 'duke-yin-helper' ),
 		'id'   => '_lrc',
 		'type' => 'file',
+		'text'    => array('add_upload_file_text' => esc_html__( 'Add file', 'duke-yin-helper' )),
 	) );
 
 }
